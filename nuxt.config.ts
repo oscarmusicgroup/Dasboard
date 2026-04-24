6c6874937109f58955ed1b32350ce4ff6ec39f0e
Robert <robert@Roberts-MacBook-Air.local>
2026-04-25 00:04:49 +0700

    chore: setup cloudflare pages preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,18 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
+  app: {
+    head: {
+      title: 'Oscar Music Group | Dashboard Coming Soon',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { charset: 'utf-16' },
+        { name: 'description', content: 'Hệ thống Dashboard dành riêng cho Nghệ sĩ của Oscar Music Group đang được phát triển.' }
+      ]
+    }
+  }
 })
